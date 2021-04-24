c5fb4b7114a9a6281d69a5763962bee57adeada2
Cody Brunner <rockchalkwushock@icloud.com>
2021-04-24 07:52:22 -0500

    feat(_app): sets up react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import '../styles/global.scss'
 
 interface Props extends AppProps {}
 
 const App: React.FC<Props> = ({ Component, pageProps }) => {
+  const queryClientRef = React.useRef<QueryClient>()
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
+
   return (
-    <div className="container flex flex-col flex-grow items-center justify-center mx-auto relative w-full">
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClientRef.current}>
+      <div className="container flex flex-col flex-grow items-center justify-center mx-auto relative w-full">
+        <Component {...pageProps} />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
